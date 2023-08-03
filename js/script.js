7c05e09fc5d1ef1aaf417b2121ff9e34459fb8be
Andrzej Sterniewski <drapiszon@gmail.com>
2023-08-03 22:05:25 +0200

    add functions: getMoveName & displayResult in script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function getMoveName(argMoveId){
     if(argMoveId == 1){
-      return 'kamień';
-    } else if(argMoveId == 2) {
+        return 'kamień';
+    } else if(argMoveId == 2){
         return 'papier';
     } else if(argMoveId == 3){
         return 'nożyce';
@@ -9,10 +9,8 @@ function getMoveName(argMoveId){
     return 'nieznany ruch';
   }
 
-function displayResult(argComputerMove, argPlayerMove){
-    if(argPlayerMove == 'nieznany ruch'){
-        printMessage('nieznany ruch, spróbuj ponownie');
-    } else {
+function displayResult(argComputerMove, argPlayerMove) {
+    console.log('moves:', argComputerMove, argPlayerMove);
     if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
         printMessage('Ty wygrywasz!');
     } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
@@ -25,7 +23,7 @@ function displayResult(argComputerMove, argPlayerMove){
         printMessage('Ja wygrałem!');
     } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
         printMessage('Ja wygrałem!');
-    } else if (argComputerMove == 'kamień' && argPlayerMovee == 'nożyce'){
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
         printMessage('Ty wygrywasz!');
     }
     
@@ -35,7 +33,6 @@ function displayResult(argComputerMove, argPlayerMove){
         printMessage('Jest remis!');
     } else if(argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
         printMessage('Jest remis!');
-    }
     }
   }
 
@@ -49,18 +46,10 @@ printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1') {
-    playerMove = 'kamień';
-} else if(playerInput == '2') {
-    playerMove = 'papier';
-} else if(playerInput == '3') {
-    playerMove = 'nożyce';
-} else {
-    playerMove = 'nieznany ruch';
-}
+let playerMove = getMoveName(playerInput);
 
 console.log('Gracz wpisał:' + playerInput);
 
 printMessage('Twój ruch to: ' + playerMove);
+
+displayResult(computerMove, playerMove);
